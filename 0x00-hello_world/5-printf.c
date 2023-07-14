83adcaf87259948f803cbedd55819a92e3388b7d
zzombii <zombixblack403@gmail.com>
2023-07-13 19:36:30 -0700

    i did it

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program that prits with printf founction.
+ *
+ * Return: Alawys 0 (Success)
+ */
+
+int main(void)
+{
+	pritf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
