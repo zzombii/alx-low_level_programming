fa279950aec91e2c64a4c336e52fb5815310be15
zzombii <zombixblack403@gmail.com>
2023-07-20 05:59:52 -0700

    i did it

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - Entry poitn
+ *
+ * Description: print alphabet usint _putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void);
+{
+
+	char i = 'a';
+
+	while (i <= 'z')
+	{
+		_putchar(i);
+		i++;
+	}
+		_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,5 +5,5 @@
 
 int _putchar(char c);
 
-
+void print_alphabet(void);
 #endif
