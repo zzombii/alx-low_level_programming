b96f3d93fab48c98400251e6f779eddfca1d6473
zombi <zombixblack403@gmail.com>
2023-07-20 17:58:44 +0300

    i did it

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_alphabet -utilizes on the _putcahr function to print the alphabet a - z
+ * print_alphabet - utilizes on the _putcahr function to print the alphabet
 */
 
 void print_alphabet(void)
