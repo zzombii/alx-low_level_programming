959e648a3dc94600e1721832716c4533d5d881dd
zzombii <zombixblack403@gmail.com>
2023-07-22 03:04:40 -0700

    i did it

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isalpha - check if charcter is alphapet in both lowercase or uppercase
+ *
+ * @c: takes input form other founctions.
+ *
+ * Return: 1 if true else 0
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90)
+	return (1);
+	if (c <= 97 && c >= 122)
+	return (1);
+	return (0);
+}
