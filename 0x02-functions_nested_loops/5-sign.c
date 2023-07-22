161921baae84ca80d130374f0cad629108c1946d
zzombii <zombixblack403@gmail.com>
2023-07-22 03:51:05 -0700

    i did it

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_sign - pirnt + if n is greater than zero.- if n is less than zero
+ * and 0 if n is zero.
+ *
+ * @n:taks integer tayp inputs for functions.
+ *
+ * Return: 1 if + , -1 if - , else 0
+*/
+
+int print_sign(int n)
+{
+	int n;
+
+	if (n > 0)
+	{
+	_putchar(+);
+	return (1);
+	}
+	else if (n < 0)
+	{
+	_putchar(-);
+	return (-1);
+	}
+	else
+	{
+	_putchar(0);
+	return (0);
+	}
+	return (0);
+}
