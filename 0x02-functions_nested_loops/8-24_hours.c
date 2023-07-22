68eefbbe9a777acc30ce44b9bec29f8b089a658e
zzombii <zombixblack403@gmail.com>
2023-07-22 11:30:57 -0700

    i did it

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,18 +6,19 @@
 */
 void jack_bauer(void)
 {
-int min, hour;
-for (hour = 0; hour <= 23; hour++)
-{
-for (min = 0; min <= 59; min++)
-{
-_putchar((hour / 10) + '0');
-_putchar((hour % 10) + '0');
-_putchar(':');
-_putchar((min / 10) + '0');
-_putchar((min % 10) + '0');
-_putchar(10);
-}
-}
+	int min, hour;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+	for (min = 0; min <= 59; min++)
+	{
+		_putchar((hour / 10) + '0');
+		_putchar((hour % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar(10);
+	}
+	}
 
 }
