c9d342fe4e1498fb43a15b2584b37c92a74edbff
zombi <zombixblack403@gmail.com>
2023-08-03 17:07:46 +0300

    i did it

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * mul - a function hat multiplies two integers.
+ * @a: is a tested int
+ * @b: ia a tested int
+ * Return: Always 0.
+ */
+
+int mul(int a, int b)
+{
+	int s;
+
+	s = a * b;
+}
