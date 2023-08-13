87db770682ab59dee589c856134801d1c1e1a4f5
zombi <zombixblack403@gmail.com>
2023-08-13 14:14:13 +0300

    i did it

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+*main - program that prints either number
+*or fizz or buzz or fizzBuzz
+*
+*
+*Return: returns 0
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+	if (i % 3 == 0 && i % 5 == 0)
+	{
+	printf("FizzBuzz ");
+	}
+	else if (i % 3 == 0)
+	{
+	printf("Fizz ");
+	}
+	else if (i % 5 == 0)
+	{
+	printf("Buzz ");
+	}
+	else
+	{
+	printf("%i ", i);
+	}
+	}
+return (0);
+}
